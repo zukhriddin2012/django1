23f1b5b6fa87376df37a2ceafad39b5cf5d7ec66
zuhriddin2012 <zuhriddin2012@gmail.com>
2020-07-11 10:06:37 +0500

    commit message

--- second_project/second_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from second_app.models import AccessRecord, Webpage, Topic
-# Register your models here.
-
-admin.site.register(AccessRecord)
-admin.site.register(Topic)
-admin.site.register(Webpage)
--- second_project/second_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from second_app.models import User
-# Create your views here.
-def index(request):
-    webpages_list = User.objects.order_by('first_name')
-    date_dict = {'access_records': webpages_list}
-    return render(request, 'second_app/index.html', context=date_dict)
